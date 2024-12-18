007af267656fc2ffe2b1b486fef6726614a35105
YugoNakamura <yugo1334@gmail.com>
2024-12-19 01:32:26 +0900

    react-leafletによる地図表示

---
+++ typescript/trainmap/src/components/Map.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet'
+import './Map.css';
+
+export const Map = () => {
+  // 緯度軽度
+  const position = [34.99, 137];
+  // 初期マップズームレベル
+  const zoom = 15;
+  return (
+    <MapContainer center={position} zoom={zoom}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  )
+};
