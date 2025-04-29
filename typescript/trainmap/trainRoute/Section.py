e3f0e8c38f4b4a4fbd562578643d7e6a7701b95c
YugoNakamura <yugo1334@gmail.com>
2025-04-30 02:13:22 +0900

    BFSクラスを追加し、Sectionクラスの初期化処理を修正。未使用のコードを削除し、セクションを駅の座標で分割する機能を強化。

--- typescript/trainmap/trainRoute/Section.py
+++ typescript/trainmap/trainRoute/Section.py
@@ -1,95 +1,15 @@
 import json
 import numpy as np
+import BFS
 class Section:
     def __init__(self, railRoads, startPoint, stations):
-        # 既に探索したindex
-        self.searchedIndexes = []
-        # 探索予定のrailIndex
-        self.searchIndexes = []
+        bfs = BFS.BFS(railRoads, startPoint)
         # 区間情報
-        self.coords = []
+        self.coords = bfs.start()
         # 路線区間情報
         self.sections = []
-        # 未加工の路線情報
-        self.railRoads = railRoads
-        # 探索を開始する座標(路線の末端)
-        self.startPoint = startPoint
         # sectionを駅の座標で分割する
         self.stations = stations
-
-    # 指定した座標から始まる/終わる要素がいくつあるか
-    def countRailload(self, coord):
-        count = 0
-        for i in range(len(self.railRoads)):
-            tmpCoords = self.railRoads[i]['geometry']['coordinates']
-            if tmpCoords[0] == coord or tmpCoords[-1] == coord:
-                count += 1
-        # 自分自身の分を引く
-        return count-1
-    
-    def searchUnUsedRailRoad(self, coord):
-        indexes = []
-        for i in range(len(self.railRoads)):
-            tmpCoords = self.railRoads[i]['geometry']['coordinates']
-            # [railRoads内で座標が一致しているindex, 先頭か末尾か]
-            # 先頭の座標が一致した
-            if tmpCoords[0] == coord and i not in self.searchedIndexes:
-                indexes.append([i, 0])
-            # 末尾の座標が一致した
-            if tmpCoords[-1] == coord and i not in self.searchedIndexes:
-                indexes.append([i, -1])
-        return indexes
-    
-    # 指定したRailRoadの座標から接続数1の区間を連結させ，sectionに変換
-    def connectRailRoad(self, railIndex, coordIndex):
-        section = []
-        while True:
-            # 探索済みリストに追加
-            self.searchedIndexes.append(railIndex)
-            tmpCoords = self.railRoads[railIndex]['geometry']['coordinates']
-            if coordIndex == 0:
-                for j in range(len(tmpCoords)):
-                    section.append([tmpCoords[j][1], tmpCoords[j][0]])
-            else:
-                for j in reversed(range(len(tmpCoords))):
-                    section.append([tmpCoords[j][1], tmpCoords[j][0]])
-
-            # railRoads[railIndex]の後が分岐しているか
-            count = self.countRailload(section[-1])
-            if count !=1:
-                self.coords.append(section)
-                return [section[-1][1], section[-1][0]]
-            else:
-                indexes = self.searchUnUsedRailRoad(section[-1])
-                # railIndexとcoordIndexを更新
-                railIndex = indexes[0][0]
-                coordIndex = indexes[0][1]
-                # sectionの末尾を削除
-                section = section[:-1]
-
-    # 幅優先探索で路線の座標をまとめる
-    def bsf(self):
-        # 区間情報を生成
-        # 路線の末端の座標
-        startCoord = self.startPoint
-        indexes = self.searchUnUsedRailRoad(startCoord)
-        self.searchIndexes.append(indexes[0])
-
-        while True:
-            nodeIndex = self.searchIndexes.pop(0)
-            lastCoord = self.connectRailRoad(nodeIndex[0], nodeIndex[1])
-            # serachedIndexesにもsearchIndexesにもないindexをsearchIndexesに追加
-            indexes = self.searchUnUsedRailRoad(lastCoord)
-            for index in indexes:
-                match = False
-                # 既に探索済みのindexと一致しているか
-                for searchIndex in self.searchIndexes:
-                    if index[0] == searchIndex[0]:
-                        match = True
-                if match == False:
-                    self.searchIndexes.append(index)
-            if len(self.searchIndexes) == 0:
-                break
 
     def getDistance(self, coord1, coord2):
         # 緯度経度から距離を計算する
@@ -128,7 +48,6 @@ class Section:
             return -1
 
     def start(self):
-        self.bsf()
         self.setCoordDirection()
         self.splitSections()
 
